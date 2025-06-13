414bb95f0f78da348e00973ece79c43c5b97660b
Vectormusmax <abcnewsa8@gmail.com>
2025-06-12 22:53:11 -0700

    fix

--- js/index-C6Bl0LaG.js
+++ js/index-C6Bl0LaG.js
@@ -8779,7 +8779,7 @@ function Yi({
     const t = [{
         name: "telegram",
         icon: c.jsx(eh, {}),
-        url: "https://t.me/PeaceCoinEth"
+        url: "https://t.me/worldpeacecoinoneth"
     }, {
         name: "twitter",
         icon: c.jsx(th, {}),
